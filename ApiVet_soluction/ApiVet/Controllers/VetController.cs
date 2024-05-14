1958f7971c406d7cb53f8d5dd52ad8e4c4572324
BraulioOsorio <braulioosoriomartinez@gmail.com>
2024-05-14 10:20:30 -0500

    s

---
+++ ApiVet_soluction/ApiVet/Controllers/VetController.cs
@@ -0,0 +1,21 @@
+﻿using ApiVet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VetController : ControllerBase
+    {
+        [HttpGet]
+
+        public IEnumerable<Vet> GetVets() {
+            return new List<Vet>
+            {
+                new Vet{id=1,nombre="Probando"},
+                new Vet{id=2,nombre="Vista a la playa"}
+            };
+        }
+    }
+}
---
+++ ApiVet_soluction/ApiVet/Models/Vet.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiVet.Models
+{
+    public class Vet
+    {
+        public int id { get; set; }
+
+        public string nombre { get; set; }
+
+
+
+    }
+}
